5bef3e2424036e40782f00eb280e6388dc8119aa
Maks <unreal9981@gmail.com>
2024-09-21 23:58:49 +0300

    wip

--- src/portalAPI.js
+++ src/portalAPI.js
@@ -114,7 +114,7 @@ function formatDate(timestamp) {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours()).padStart(2, '0');
+    const hours = String(date.getHours() + 1).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
 
     return `${year}-${month}-${day}T${hours}:${minutes}`;
